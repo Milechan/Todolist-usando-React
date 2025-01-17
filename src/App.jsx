1198b846f284786d6e4b902c40dfc57d0cdd57e0
milena <milena.concha.m@gmail.com>
2025-01-17 16:29:40 -0300

    componente app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Tarea from './Tarea'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputTarea = useRef(null)
+  const [tareas, setTareas] = useState([
+    { id: 1, contenido: "lavar la ropa" },
+    { id: 2, contenido: "cocinar" },
+    { id: 3, contenido: "lavar la loza" },
+  ])
+  function CrearTarea(tareaContenido) {
+    console.log(tareaContenido)
+    let nuevatarea = {
+      id: tareas.length+1,
+      contenido: tareaContenido
+    }
+    setTareas([...tareas, nuevatarea])
+    inputTarea.current.value=""
 
+  }
+  function borrarTarea(id) {
+   let tareasActualizadas=tareas.filter((tarea)=>tarea.id!==id)
+    setTareas(tareasActualizadas)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="contenedor">
+      <div class="titulo"> To Do List </div>
+      <div class='ListContainer'>
+        <input ref={inputTarea} type="text" name="tarea" id="tarea" onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            CrearTarea(inputTarea.current.value)
+          }
+        }} />
+
+        {tareas.map((tarea) => {
+          return <Tarea id={tarea.id} contenido={tarea.contenido} delete={borrarTarea}/>
+        })}
+<div className='Footer'> faltan {tareas.length} elementos</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+    </div>
+
   )
 }
 
